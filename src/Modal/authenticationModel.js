d7bef0d661bf042a0bfbf0c7e7fa23db429a4a40
Praveen Soni <praveen@shinewebservices.com>
2022-02-21 19:01:40 +0530

    cooment local

--- src/Modal/authenticationModel.js
+++ src/Modal/authenticationModel.js
@@ -7,19 +7,19 @@ const authenticationModel = {
   
   addUser: action((state, payload) => {
     state.response.push(payload)
-    localStorage.setItem("userDetail",JSON.stringify( state.response))
+    //localStorage.setItem("userDetail",JSON.stringify( state.response))
   }),
 
   setlogin: action((state, payload) => {
     console.log("paylaod",payload)
     state.reduxlogin=payload
-    localStorage.setItem("isLogin",payload)
+  //  localStorage.setItem("isLogin",payload)
   }),
 
   clear: action((state, payload) => {
     console.log("paylaod",payload)
     state.reduxlogin=payload
-    localStorage.removeItem("isLogin");
+    //localStorage.removeItem("isLogin");
   }),
 
 
